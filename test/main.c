7cd45d7dcce77dafa55aac2d9530ec7873e3c909
Tom Weatherhead <tomw@2hrd4u.org>
2022-09-28 21:58:59 -0600

    Dev

--- test/main.c
+++ test/main.c
@@ -8,8 +8,23 @@ int main(int argc, char * argv[]) {
 	const int b = 5;
 
 	const int sum = add(a, b);
+	const int diff = subtract(a, b);
 
 	printf("%d + %d = %d\n", a, b, sum);
+
+	if (sum != a + b) {
+		fprintf(stderr, "add() failed\n");
+		exit(1);
+	}
+
+	printf("%d - %d = %d\n", a, b, diff);
+
+	if (diff != a - b) {
+		fprintf(stderr, "subtract() failed\n");
+		exit(1);
+	}
+
+	printf("\nAll tests passed.\n");
 
 	return 0;
 } // End of main()
